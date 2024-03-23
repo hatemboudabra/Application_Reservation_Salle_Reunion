b8fda019b8e5a7283e2f798a02a4e7737d8d9174
mouhamedjouini <mouhadje@gmail.com>
2024-03-23 01:35:11 +0100

    integration-template

--- AppFront/src/app/app-routing.module.ts
+++ AppFront/src/app/app-routing.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { CommonModule } from '@angular/common';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardUserComponent } from './dashboard-user/dashboard-user.component';
 
 const routes: Routes = [
-  { path: '/login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  {path: '', redirectTo:'login', pathMatch: 'full'},
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboardus', component: DashboardUserComponent}, 
+  { path: 'dashboard', component: DashboardComponent , children:[
+    {path: '', redirectTo:'dashboard', pathMatch: 'full'},
+]}
+ 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
+  imports: [CommonModule,RouterModule.forRoot(routes)],
+  exports: [RouterModule]})
 export class AppRoutingModule { }
--- AppFront/src/app/app.module.ts
+++ AppFront/src/app/app.module.ts
@@ -5,16 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
+import { CommonModule, DatePipe } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './dashboard/footer/footer.component';
+import { NavbarComponent } from './dashboard/navbar/navbar.component';
+import { HeaderComponent } from './dashboard/header/header.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardUserComponent } from './dashboard-user/dashboard-user.component';
+import { NavbaruserComponent } from './dashboard-user/navbaruser/navbaruser.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    FooterComponent,
+    NavbarComponent,
+    HeaderComponent,
+    RegisterComponent,
+    DashboardUserComponent,
+    NavbaruserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
   ],
-  providers: [],
+  providers: [DatePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
