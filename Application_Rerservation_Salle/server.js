8fab2397eb3097507ada3cd8e7c9e2edcf9217e5
Your Name <you@example.com>
2024-08-03 23:20:26 +0100

    deploy with k8s

--- Application_Rerservation_Salle/server.js
+++ Application_Rerservation_Salle/server.js
@@ -2,27 +2,36 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const authRoutes  = require('./routes/auth');
-const meetingRoutes  = require('./routes/meetingRoom');
-const reservationRoutes  = require('./routes/Reservation');
+const authRoutes = require('./routes/auth');
+const meetingRoutes = require('./routes/meetingRoom');
+const reservationRoutes = require('./routes/Reservation');
+
 dotenv.config();
-//require('dotenv').config();
-const MONGODB_URI = process.env.MONGODB_URI
-const PORT = process.env.PORT || 5000
+
+const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 app.use(express.json());
-app.use(cors());
-app.use('/auth',authRoutes)
-app.use('/meetingRoom',meetingRoutes)
-app.use('/Reservation',reservationRoutes)
-app.use('/getimage',express.static('./upload/admin'))
 
-// connection to mongodb and start server 
-mongoose.connect(MONGODB_URI).then(()=>{
-    console.log('connected to MongoDb');
-    app.listen(PORT,()=>{
-        console.log(`server listening on ${PORT}`)
-    })
-}).catch((err) =>{
-    console.error('Error connecting to mongodb:',err.message)
-})
+// Allow all origins for development or specify your frontend URL
+app.use(cors({
+  origin: 'http://localhost', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.use('/auth', authRoutes);
+app.use('/meetingRoom', meetingRoutes);
+app.use('/Reservation', reservationRoutes);
+app.use('/getimage', express.static('./upload/admin'));
+
+// Connection to MongoDB and start server
+mongoose.connect(MONGODB_URI).then(() => {
+  console.log('connected to MongoDb');
+  app.listen(PORT, () => {
+    console.log(`server listening on ${PORT}`);
+  });
+}).catch((err) => {
+  console.error('Error connecting to mongodb:', err.message);
+});
